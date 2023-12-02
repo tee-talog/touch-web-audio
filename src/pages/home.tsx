b1e2f648443e48674010d263b4d815c98ba2ea36
talog <tee.talog@gmail.com>
2023-12-02 17:34:46 +0900

    キーボードでもなるようにした

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,9 +4,35 @@ import { useAudio } from "../modules/audio"
 const { startSound, stopSound } = useAudio()
 
 const Home = () => {
+  const [isPlaying, setPlaying] = createSignal(false)
+
+  const press = () => {
+    if (isPlaying()) {
+      return
+    }
+    setPlaying(true)
+    startSound()
+  }
+
+  // FIXME マウスとキーボード両方で鳴らしているとき、片方を離すと音が途切れる
+  const release = () => {
+    if (!isPlaying()) {
+      return
+    }
+    setPlaying(false)
+    stopSound()
+  }
+
   return (
     <section>
-      <button type="button" onMouseDown={startSound} onMouseUp={stopSound}>
+      <button
+        type="button"
+        onMouseDown={press}
+        onMouseUp={release}
+        onKeyDown={press}
+        onKeyUp={release}
+        onMouseLeave={release}
+      >
         play
       </button>
     </section>
